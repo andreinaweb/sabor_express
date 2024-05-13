44656873ef48d9a838191a99cecf421bfc619060
andrei.lc <andrei.lc.ti@gmail.com>
2024-05-13 01:47:33 -0300

    Atualização aula 3

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def exibir_nome_do_programa():
 ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░      
-      """) #três aspas podem substiruir o \n para quebra de linha
+    """) #três aspas podem substiruir o \n para quebra de linha
 
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
@@ -19,29 +19,27 @@ def exibir_opcoes():
     print('3. Ativar restaurante')
     print('4. Sair \n')
 
-opcao_escolhida = int(input('Escolha uma opção: '))
-#print('Você escolheu a opção: ', opcao_escolhida)
-print(f'Você escolheu a opção: {opcao_escolhida}')
-
-#print(type(opcao_escolhida))
-#print(type(1))
-
 #Criando uma função
 def finalizar_app():
     os.system('cls')#lib para passar comandos do OS
     print('Encerrando o programa \n')
 
-if opcao_escolhida == 1:
-    print('Cadastrar restaurante')
-elif opcao_escolhida == 2:
-    print('Listar restaurante')
-elif opcao_escolhida == 3:
-    print('Ativar restaurante')
-else:
-    finalizar_app()
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurante')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else:
+        finalizar_app()
 
 def main():
     exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
     
-if __name__ == __main__
+if __name__ == '__main__':
     main()
